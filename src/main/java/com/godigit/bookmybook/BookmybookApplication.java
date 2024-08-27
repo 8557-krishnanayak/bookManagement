78ae14c264069e928df4a043c122643dd67f9d3e
8557-krihsnanayak <krishna.nayak@godigit.com>
2024-08-27 10:11:46 +0530

    init

---
+++ src/main/java/com/godigit/bookmybook/BookmybookApplication.java
@@ -0,0 +1,13 @@
+package com.godigit.bookmybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godigit/bookmybook/BookmybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godigit.bookmybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
