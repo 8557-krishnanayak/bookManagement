dc1314ab56ca77c5ec89be5d44707919f95b0c1a
8557-krishnanayak <krishna.nayak@godigit.com>
2024-09-02 14:33:11 +0530

    swagger integration done

--- src/main/java/com/godigit/bookmybook/config/AppConfig.java
+++ src/main/java/com/godigit/bookmybook/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.godigit.bookmybook.config;
 
 import com.godigit.bookmybook.util.TokenUtility;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +11,13 @@ public class AppConfig {
     @Bean
     public TokenUtility tokenUtility(){
         return new TokenUtility();
+    }
+
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot Application")
+                        .version("2.0.0")
+                        .description("API documentation for Spring Boot Application"));
     }
 }
